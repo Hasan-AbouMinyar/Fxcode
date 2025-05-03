78af8bddc8b42a4ce67135ba31c9cfde5e743376
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-03 04:20:10 +0200

    add Text on group to the scene

--- src/fxcode/SceneFx.java
+++ src/fxcode/SceneFx.java
@@ -3,6 +3,8 @@ package fxcode;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -16,8 +18,15 @@ public class SceneFx extends Application {
     public void start(Stage primaryStage) throws Exception {
         
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root,600,600,Color.GREENYELLOW);
         Stage stage = new Stage();
+        
+        Text des = new Text();
+        des.setText("this is an application");
+        des.setX(50);
+        des.setY(50);
+        
+        root.getChildren().add(des);
         
         stage.setScene(scene);
         stage.show();
