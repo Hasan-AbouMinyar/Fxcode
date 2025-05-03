c7f5bb42d54e8831ef4abd67025e3d8f6b9c01b5
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-03 03:58:49 +0200

    new file for tutorial scene

--- src/fxcode/HelloFx.java
+++ src/fxcode/HelloFx.java
@@ -1,32 +1,51 @@
-
 package fxcode;
 
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloFx extends Application {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        // إنشاء مجموعة وScene
+        Group first = new Group();
+        Scene sacand = new Scene(first, Color.YELLOW);
+
+        // إضافة الأيقونة
+        Image logo = new Image("/fxcode/logo.png");
+        stage.getIcons().add(logo);
+
+        // تعيين المشهد أولاً
+        stage.setScene(sacand);
+        stage.setTitle("yellow scene");
         
-        //Stage go = new Stage();
-      Group first = new Group();
-      
-      Scene sacand = new Scene(first,Color.YELLOW);
-      
-      stage.setScene(sacand);
-      
-      stage.setTitle("yellow scene");
-      stage.show();
         
+//        stage.setWidth(600);
+//        stage.setHeight(600);
+//      
+//        stage.setX(50);
+//        stage.setY(50);
+
+        
+
+        // تعيين إعدادات الخروج من fullscreen
+        stage.setFullScreenExitHint("You have to hit H key on your keyboard");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("H"));
+
+        // تفعيل fullscreen بعد تعيين المشهد
+        stage.setFullScreen(true);
+
+        // إظهار الشاشة
+        stage.show();
     }
-    
-    
 }
