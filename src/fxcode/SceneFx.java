71f67fff3ba61041e01dfddd4e6a42767371f373
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-03 04:11:00 +0200

    create fx page whit mySelf

---
+++ src/fxcode/SceneFx.java
@@ -0,0 +1,27 @@
+package fxcode;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class SceneFx extends Application {
+    
+    public static void main(String[] args){
+       launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    
+}
