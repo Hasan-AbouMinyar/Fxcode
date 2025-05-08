ac84987413a798c54f753506f84a60d6ecc89d52
Hasan AbouMinyar <among.00.12@gmail.com>
2025-05-08 03:13:52 +0200

    just

--- src/fxcode/SceneG.java
+++ src/fxcode/SceneG.java
@@ -4,6 +4,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 public class SceneG extends Application {
 
  
@@ -16,9 +21,32 @@ public class SceneG extends Application {
         
         Group hold = new Group();
         
-        Scene tohold = new Scene(hold);
+        Scene tohold = new Scene(hold,600,600,Color.BLUEVIOLET);
+        
+        Text whooa = new Text();
+        whooa.setText("whoooa whoooa");
+        whooa.setX(50);
+        whooa.setY(50);
+        whooa.setFont(Font.font("Verdana",60));
+        whooa.setFill(Color.WHITESMOKE);
+        
+        Line le = new Line();
+        le.setStartX(0);
+        le.setStartY(60);
+        le.setEndX(600);
+        le.setEndY(60);
+        
+        le.setStrokeWidth(4);
+        le.setStroke(Color.WHITESMOKE);
+        le.setOpacity(0.2);
+        
+        hold.getChildren().add(le);
+        
         
         ex.setScene(tohold);
+        
+        
+        hold.getChildren().add(whooa);
         
         ex.show();
         
