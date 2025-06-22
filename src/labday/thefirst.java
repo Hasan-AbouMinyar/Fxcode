0e985bf693c89c3008a6b0d456d795ea213f5fbf
Hasan AbouMinyar <among.00.12@gmail.com>
2025-06-22 04:42:43 +0200

    new pac

---
+++ src/labday/thefirst.java
@@ -0,0 +1,18 @@
+
+package labday;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+        
+public class thefirst extends Application {
+    
+    @Override
+    public void start(Stage ste){
+        
+    }
+    
+    public static void main (String[] args){
+        launch();
+    }
+    
+}
