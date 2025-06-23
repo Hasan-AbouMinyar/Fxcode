7d1faee48ed4b223ba18ed6b6c6fc44d2bb659b8
Hasan AbouMinyar <among.00.12@gmail.com>
2025-06-23 04:43:54 +0200

    arc save

--- src/labday/ARC.java
+++ src/labday/ARC.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package labday;
 
-/**
- *
- * @author abouminyar
- */
-public class ARC {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+ 
+
+public class ARC extends Application{
+
+  @Override
+    public void start(Stage PStage){
+        Group root = new Group();
+        
+        Arc one = new Arc(200,200,100,100,45,240);
+        one.setType(ArcType.ROUND);
+//        one.setFill(Color.TRANSPARENT);
+        one.setStroke(Color.BLACK);
+        
+        root.getChildren().add(one);
+        
+        Scene scene = new Scene(root,400,400);
+        PStage.setScene(scene);
+        PStage.show();
+        
+        
+    }
+    
+    public static void main (String[] args){
+        launch();
+    }
     
 }
+
