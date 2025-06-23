c15c3c9beb460cb66d1206b1d9c735b3a23d8fe4
Hasan AbouMinyar <among.00.12@gmail.com>
2025-06-23 02:44:06 +0200

    Ellipse Black yoo

--- src/labday/Elips.java
+++ src/labday/Elips.java
@@ -1,18 +1,28 @@
-
 package labday;
-
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.paint.Color;
  
 
-public class tamplet extends Application{
+public class Elips extends Application{
 
   @Override
     public void start(Stage PStage){
         Group root = new Group();
+        
+        Ellipse holl = new Ellipse();
+        holl.setCenterX(200);
+        holl.setCenterY(200);
+        holl.setRadiusX(100);
+        holl.setRadiusY(50);
+        holl.setFill(Color.BLACK);
+        holl.setStroke(Color.RED);
+        holl.setStrokeWidth(4);
+        root.getChildren().add(holl);
         
         
         Scene scene = new Scene(root,400,400);
@@ -27,4 +37,3 @@ public class tamplet extends Application{
     }
     
 }
-
