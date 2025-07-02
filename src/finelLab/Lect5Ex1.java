4578c3a3c97a5c2c21786d39b5c637a5585d6f06
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-02 09:05:57 +0200

    run file

--- src/finelLab/Lect5Ex1.java
+++ src/finelLab/Lect5Ex1.java
@@ -1,28 +1,34 @@
-
 package finelLab;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class NewClass extends Application {
-    
+public class Lect5Ex1 extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
-    public void start(Stage g){
-        
-        Group amg = new Group();
-        
-        Scene doe = new Scene(amg,400,400);
-        
-        g.setScene(doe);
-        
-        g.show();
-        
+    public void start(Stage stage) {
+        Button yesBtn = new Button("Yes");
+        Button noBtn = new Button("No");
+        Button cancelBtn = new Button("Cancel");
+
+        HBox root = new HBox();
+        root.getChildren().addAll(yesBtn, noBtn, cancelBtn);
+
+        Scene scene = new Scene(root,400,400);
+        // Add a style sheet to the scene
+//        scene.getStylesheets().add("finalLab/styles.css");
+        scene.getStylesheets().add(getClass().getResource("/finelLab/styles.css").toExternalForm());
+
+
+        stage.setScene(scene);
+        stage.setTitle("Styling Buttons");
+        stage.show();
     }
-    
-    public static void main(String [] args){
-        launch();
-    }
-    
 }
