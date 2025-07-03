a161db2bb789ac43d8d40de9d282f4a29504c917
Hasan AbouMinyar <among.00.12@gmail.com>
2025-07-03 05:25:47 +0200

    The same point

--- src/AllOne/RadialGradientExample.java
+++ src/AllOne/RadialGradientExample.java
@@ -3,11 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package AllOne;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-/**
- *
- * @author abouminyar
- */
-public class NewClass {
-    
+public class RadialGradientExample extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // إنشاء حاوية
+        Group G = new Group();
+
+        // إنشاء كائن التدرج اللوني الدائري
+        // Radial gradient color 
+        RadialGradient gradient1 = new RadialGradient(0,
+                .0,
+                .1,
+                .1,
+                .9,
+                true,
+                CycleMethod.NO_CYCLE,
+                new Stop(0, Color.RED),
+                new Stop(1, Color.BLACK)); 
+
+        // إنشاء دائرة وتطبيق التدرج عليها
+        Circle c1 = new Circle(150, 150, 50); 
+        c1.setFill(gradient1);
+
+        G.getChildren().add(c1); 
+
+        // إعداد وعرض النافذة
+        Scene scene = new Scene(G, 300, 300);
+        primaryStage.setTitle("Radial Gradient!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
